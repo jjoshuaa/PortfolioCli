6bb9a8f4af7ae3174a91aea929cc4d5a29f74df4
jjoshuaa <87516124+jjoshuaa@users.noreply.github.com>
2021-11-21 20:13:08 +0300

    Update cli.js

--- cli.js
+++ cli.js
@@ -1,34 +1,62 @@
-import React from 'react';
-import {render} from 'ink';
-import SelectInput from 'ink-select-input';
+import React from 'react'
+const { render, Box, Text } = require("ink");
+const BigText = require("ink-big-text");
+const Gradient = require("ink-gradient");
+const SelectInput = require("ink-select-input").default;
+const open = require("open");
 
-const Demo = () => {
-	const handleSelect = item => {
-		// `item` = { label: 'First', value: 'first' }
-	};
+const handleSelect = (item) => {
+  if (item.url) {
+    open(item.url);
+  }
 
-	const items = [
+  if (item.action) {
+    item.action();
+  }
+};
+
+const createItems = (items) => {
+  for (const item of items) {
+    item.key = item.url || item.label;
+  }
+
+  return items;
+};
+	const items =createItems([
 		{
-			label: '🏡 Website  ',
+			label: '🏡 Website (Work In Progress) ',
 			value: 'first'
 		},
 		{
 			label: '📬 Email',
-			value: 'second'
+			url: 'mailto:joshiirat@gmail.com.com'
 		},
 		{
 			label: '💾 Github',
-			value: 'third'
+			url:'https://github.com/jjoshuaa'
 		},
     {
-      label: "🚪  Quit",
+      label: "🚪 Quit",
       action() {
        process.exit();
       }
     }
-	];
+	]);
 
-	return <SelectInput items={items} onSelect={handleSelect} />;
-};
-
-render(<Demo />);
+const Menu = () => (
+    <Box flexDirection="column" marginTop={1}>
+      <Box flexDirection="column" alignItems="center">
+        <Gradient name="rainbow">
+          <BigText text="Joshua" font="simple3d" space={false} />
+        </Gradient>
+        <Box marginY={1}>
+          <Text>Just another slacker bringing ideas to fruition :)</Text>
+        </Box>
+      </Box>
+      <Box marginLeft={1}>
+        <SelectInput items={items} onSelect={handleSelect} />
+      </Box>
+    </Box>
+  );
+  
+  render(<Menu />);
