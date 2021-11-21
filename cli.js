ce48d8c8b33f4a91b8d36d75ee8f505dea1e25aa
jjoshuaa <87516124+jjoshuaa@users.noreply.github.com>
2021-11-21 19:37:33 +0300

    Update cli.js

--- cli.js
+++ cli.js
@@ -1,41 +1,34 @@
-import React, { Component } from "react";
-import { render, Box, Color, AppContext, Static } from "ink";
-import SelectInput from "ink-select-input";
-import packageJson from "./package.json";
-import child_process from "child_process";
-class Demo extends Component {
-  render() {
-    const items = Object.keys(packageJson.dependencies).map((commandName) => {
-      return {
-        label: commandName,
-        value: commandName,
-      };
-    });
-    return (
-      <>
-        <Static>
-          <Box width={4} height={2}>
-            <Color greenBright>npm run command selector!</Color>
-          </Box>
-        </Static>
- 
-        <AppContext.Consumer>
-          {({ exit }) => (
-            <SelectInput
-              items={items}
-              onSelect={({ value }) => {
-                console.log("You have chosen me, Best of Luck");
-                child_process.execSync(`npm start ${value}`, {
-                  stdio: "inherit",
-                });
-                exit();
-              }}
-            />
-          )}
-        </AppContext.Consumer>
-      </>
-    );
-  }
-}
- 
+import React from 'react';
+import {render} from 'ink';
+import SelectInput from 'ink-select-input';
+
+const Demo = () => {
+	const handleSelect = item => {
+		// `item` = { label: 'First', value: 'first' }
+	};
+
+	const items = [
+		{
+			label: '🏡 Website  ',
+			value: 'first'
+		},
+		{
+			label: '📬 Email',
+			value: 'second'
+		},
+		{
+			label: '💾 Github',
+			value: 'third'
+		},
+    {
+      label: "🚪  Quit",
+      action() {
+       process.exit();
+      }
+    }
+	];
+
+	return <SelectInput items={items} onSelect={handleSelect} />;
+};
+
 render(<Demo />);
